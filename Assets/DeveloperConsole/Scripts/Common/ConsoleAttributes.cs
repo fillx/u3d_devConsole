18168284439e3c52a4c5a3b2cb829b1138be7ad5
Fill <>
2021-08-04 18:25:08 +0300

    Init

---
+++ Assets/DeveloperConsole/Scripts/Common/ConsoleAttributes.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace DeveloperConsole
+{
+    [System.AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public class ConsoleCommandAttribute : Attribute
+    {
+
+        public readonly string name, description;
+        public readonly bool PrefixOnly;
+
+        public ConsoleCommandAttribute(string name, string description, bool prefixOnly = false)
+        {
+            this.name = name;
+            this.description = description;
+            this.PrefixOnly = prefixOnly;
+        }
+
+        public ConsoleCommandAttribute(string name, bool prefixOnly = false)
+        {
+            this.name = name;
+            this.description = "no description";
+            this.PrefixOnly = prefixOnly;
+        }
+    }
+
+
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class ConsoleParseAttribute : Attribute
+    {
+    }
+}
+
+
